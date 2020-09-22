14dbb4cfee85f46127f50a99853eeb973c43b56e
Davide Todorov <davide.todorov@abv.bg>
2020-09-22 17:00:54 +0300

    Edit typos

--- Java-Advanced/Exam Preparation/Snake/src/Main.java
+++ Java-Advanced/Exam Preparation/Snake/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
         String[][] matrix = new String[size][];
 
-        for (int rol = 0; rol < size; rol++) {
-            matrix[rol] = scanner.nextLine().split("");
+        for (int row = 0; row < size; row++) {
+            matrix[row] = scanner.nextLine().split("");
         }
 
         int foodQuantity = 0;
@@ -21,24 +21,24 @@ public class Main {
 
             switch (input) {
                 case "up":
-                    for (int rol = 0; rol < matrix.length; rol++) {
-                        for (int col = 0; col < matrix[rol].length; col++) {
-                            String curr = matrix[rol][col];
+                    for (int row = 0; row < matrix.length; row++) {
+                        for (int col = 0; col < matrix[row].length; col++) {
+                            String curr = matrix[row][col];
 
                             if (curr.equals("S")) {
-                                int rollIndex = rol - 1;
+                                int rollIndex = row - 1;
                                 if (rollIndex >= 0) {
-                                    if (matrix[rol - 1][col].equals("B")) {
-                                        int[] coordinates = findNextB(matrix, rol - 1, col);
-                                        matrix[rol - 1][col] = ".";
-                                        matrix[rol][col] = ".";
+                                    if (matrix[row - 1][col].equals("B")) {
+                                        int[] coordinates = findNextB(matrix, row - 1, col);
+                                        matrix[row - 1][col] = ".";
+                                        matrix[row][col] = ".";
                                         matrix[coordinates[0]][coordinates[1]] = "S";
                                         done = true;
                                         break;
-                                    } else if (matrix[rol - 1][col].equals("*")) {
+                                    } else if (matrix[row - 1][col].equals("*")) {
                                         foodQuantity++;
-                                        matrix[rol][col] = ".";
-                                        matrix[rol - 1][col] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row - 1][col] = "S";
                                         if (foodQuantity == 10) {
                                             System.out.println("You won! You fed the snake.");
                                             System.out.println("Food eaten: 10");
@@ -53,15 +53,15 @@ public class Main {
                                         done = true;
                                         break;
                                     } else {
-                                        matrix[rol][col] = ".";
-                                        matrix[rol - 1][col] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row - 1][col] = "S";
                                         done = true;
                                         break;
                                     }
                                 } else {
                                     System.out.println("Game over!");
                                     System.out.println("Food eaten: " + foodQuantity);
-                                    matrix[rol][col] = ".";
+                                    matrix[row][col] = ".";
                                     for (String[] strings : matrix) {
                                         for (String string : strings) {
                                             System.out.print(string);
@@ -79,24 +79,24 @@ public class Main {
                     break;
 
                 case "down":
-                    for (int rol = 0; rol < matrix.length; rol++) {
-                        for (int col = 0; col < matrix[rol].length; col++) {
-                            String curr = matrix[rol][col];
+                    for (int row = 0; row < matrix.length; row++) {
+                        for (int col = 0; col < matrix[row].length; col++) {
+                            String curr = matrix[row][col];
 
                             if (curr.equals("S")) {
-                                int rollIndex = rol + 1;
+                                int rollIndex = row + 1;
                                 if (rollIndex < matrix.length) {
-                                    if (matrix[rol + 1][col].equals("B")) {
-                                        int[] coordinates = findNextB(matrix, rol + 1, col);
-                                        matrix[rol + 1][col] = ".";
-                                        matrix[rol][col] = ".";
+                                    if (matrix[row + 1][col].equals("B")) {
+                                        int[] coordinates = findNextB(matrix, row + 1, col);
+                                        matrix[row + 1][col] = ".";
+                                        matrix[row][col] = ".";
                                         matrix[coordinates[0]][coordinates[1]] = "S";
                                         done = true;
                                         break;
-                                    } else if (matrix[rol + 1][col].equals("*")) {
+                                    } else if (matrix[row + 1][col].equals("*")) {
                                         foodQuantity++;
-                                        matrix[rol][col] = ".";
-                                        matrix[rol + 1][col] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row + 1][col] = "S";
                                         if (foodQuantity == 10) {
                                             System.out.println("You won! You fed the snake.");
                                             System.out.println("Food eaten: 10");
@@ -111,15 +111,15 @@ public class Main {
                                         done = true;
                                         break;
                                     } else {
-                                        matrix[rol][col] = ".";
-                                        matrix[rol + 1][col] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row + 1][col] = "S";
                                         done = true;
                                         break;
                                     }
                                 } else {
                                     System.out.println("Game over!");
                                     System.out.println("Food eaten: " + foodQuantity);
-                                    matrix[rol][col] = ".";
+                                    matrix[row][col] = ".";
                                     for (String[] strings : matrix) {
                                         for (String string : strings) {
                                             System.out.print(string);
@@ -137,24 +137,24 @@ public class Main {
                     break;
 
                 case "left":
-                    for (int rol = 0; rol < matrix.length; rol++) {
-                        for (int col = 0; col < matrix[rol].length; col++) {
-                            String curr = matrix[rol][col];
+                    for (int row = 0; row < matrix.length; row++) {
+                        for (int col = 0; col < matrix[row].length; col++) {
+                            String curr = matrix[row][col];
 
                             if (curr.equals("S")) {
                                 int colIndex = col - 1;
                                 if (colIndex >= 0) {
-                                    if (matrix[rol][col - 1].equals("B")) {
-                                        int[] coordinates = findNextB(matrix, rol, col - 1);
-                                        matrix[rol][col - 1] = ".";
-                                        matrix[rol][col] = ".";
+                                    if (matrix[row][col - 1].equals("B")) {
+                                        int[] coordinates = findNextB(matrix, row, col - 1);
+                                        matrix[row][col - 1] = ".";
+                                        matrix[row][col] = ".";
                                         matrix[coordinates[0]][coordinates[1]] = "S";
                                         done = true;
                                         break;
-                                    } else if (matrix[rol][col - 1].equals("*")) {
+                                    } else if (matrix[row][col - 1].equals("*")) {
                                         foodQuantity++;
-                                        matrix[rol][col] = ".";
-                                        matrix[rol][col - 1] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row][col - 1] = "S";
                                         if (foodQuantity == 10) {
                                             System.out.println("You won! You fed the snake.");
                                             System.out.println("Food eaten: 10");
@@ -169,15 +169,15 @@ public class Main {
                                         done = true;
                                         break;
                                     } else {
-                                        matrix[rol][col] = ".";
-                                        matrix[rol][col - 1] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row][col - 1] = "S";
                                         done = true;
                                         break;
                                     }
                                 } else {
                                     System.out.println("Game over!");
                                     System.out.println("Food eaten: " + foodQuantity);
-                                    matrix[rol][col] = ".";
+                                    matrix[row][col] = ".";
                                     for (String[] strings : matrix) {
                                         for (String string : strings) {
                                             System.out.print(string);
@@ -195,24 +195,24 @@ public class Main {
                     break;
 
                 case "right":
-                    for (int rol = 0; rol < matrix.length; rol++) {
-                        for (int col = 0; col < matrix[rol].length; col++) {
-                            String curr = matrix[rol][col];
+                    for (int row = 0; row < matrix.length; row++) {
+                        for (int col = 0; col < matrix[row].length; col++) {
+                            String curr = matrix[row][col];
 
                             if (curr.equals("S")) {
                                 int colIndex = col + 1;
-                                if (colIndex < matrix[rol].length) {
-                                    if (matrix[rol][col + 1].equals("B")) {
-                                        int[] coordinates = findNextB(matrix, rol, col + 1);
-                                        matrix[rol][col + 1] = ".";
-                                        matrix[rol][col] = ".";
+                                if (colIndex < matrix[row].length) {
+                                    if (matrix[row][col + 1].equals("B")) {
+                                        int[] coordinates = findNextB(matrix, row, col + 1);
+                                        matrix[row][col + 1] = ".";
+                                        matrix[row][col] = ".";
                                         matrix[coordinates[0]][coordinates[1]] = "S";
                                         done = true;
                                         break;
-                                    } else if (matrix[rol][col + 1].equals("*")) {
+                                    } else if (matrix[row][col + 1].equals("*")) {
                                         foodQuantity++;
-                                        matrix[rol][col] = ".";
-                                        matrix[rol][col + 1] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row][col + 1] = "S";
                                         if (foodQuantity == 10) {
                                             System.out.println("You won! You fed the snake.");
                                             System.out.println("Food eaten: 10");
@@ -227,15 +227,15 @@ public class Main {
                                         done = true;
                                         break;
                                     } else {
-                                        matrix[rol][col] = ".";
-                                        matrix[rol][col + 1] = "S";
+                                        matrix[row][col] = ".";
+                                        matrix[row][col + 1] = "S";
                                         done = true;
                                         break;
                                     }
                                 } else {
                                     System.out.println("Game over!");
                                     System.out.println("Food eaten: " + foodQuantity);
-                                    matrix[rol][col] = ".";
+                                    matrix[row][col] = ".";
                                     for (String[] strings : matrix) {
                                         for (String string : strings) {
                                             System.out.print(string);
@@ -255,13 +255,13 @@ public class Main {
         }
     }
 
-    private static int[] findNextB(String[][] matrix, int roll, int col) {
+    private static int[] findNextB(String[][] matrix, int row, int col) {
         int rollCoordinates = 0;
         int colCoordinates = 0;
 
         for (int rol = 0; rol < matrix.length; rol++) {
             for (int cols = 0; cols < matrix.length; cols++) {
-                if (matrix[rol][cols].equals("B") && rol != roll && cols != col) {
+                if (matrix[rol][cols].equals("B") && rol != row && cols != col) {
                     rollCoordinates = rol;
                     colCoordinates = cols;
                     return new int[]{rollCoordinates, colCoordinates};
