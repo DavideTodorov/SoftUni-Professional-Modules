f86a6e26a056bc183093f31d4c6f19e53a3bcf6a
Davide Todorov <davide.todorov@abv.bg>
2020-09-27 10:54:18 +0300

    Finished Periodic Table

--- Java-Advanced/Sets And Maps/Periodic Table/src/Main.java
+++ Java-Advanced/Sets And Maps/Periodic Table/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int inputLines = Integer.parseInt(scanner.nextLine());
+
+        Set<String> elements = new TreeSet<>();
+
+
+        for (int i = 0; i < inputLines; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            for (int elementsInput = 0; elementsInput < tokens.length; elementsInput++) {
+                String currElement = tokens[elementsInput];
+                elements.add(currElement);
+            }
+        }
+
+        System.out.println(String.join(" ", elements));
     }
 }
