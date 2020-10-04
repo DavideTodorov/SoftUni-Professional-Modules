a01e78a3d15ae910b22647c523f9d4db1432b433
Davide Todorov <davide.todorov@abv.bg>
2020-10-04 13:56:29 +0300

    Created Constructors

--- Java-Advanced/Defining Classes/Car/src/Car.java
+++ Java-Advanced/Defining Classes/Car/src/Car.java
@@ -1,17 +1,27 @@
+import java.io.CharArrayReader;
+
 public class Car {
 
     private String brand;
     private String model;
     private int horsePower;
 
-    public Car(String made, String model, int horsePower) {
-        this.brand = made;
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
         this.model = model;
         this.horsePower = horsePower;
     }
 
-    public Car() {
-        this("n/a", "n/a", 0);
+    public Car(String brand) {
+        this(brand, "unknown", -1);
+    }
+
+    public Car(String brand, String model) {
+        this(brand, model, -1);
+    }
+
+    public Car(String brand, int horsePower) {
+        this(brand, "unknown", horsePower);
     }
 
     public void setBrand(String made) {
--- Java-Advanced/Defining Classes/Car/src/Main.java
+++ Java-Advanced/Defining Classes/Car/src/Main.java
@@ -6,6 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int inputCount = Integer.parseInt(scanner.nextLine());
+
+
+        List<Car> cars = new ArrayList<>();
+
+        for (int i = 0; i < inputCount; i++) {
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            int tokensSize = tokens.length;
+
+            Car car = null;
+
+            switch (tokensSize) {
+                case 1:
+                    car = new Car(tokens[0]);
+                    break;
+
+                case 2:
+                    car = new Car(tokens[0], tokens[1]);
+                    break;
+
+                case 3:
+                    car = new Car(tokens[0], tokens[1], Integer.parseInt(tokens[2]));
+                    break;
+            }
+            cars.add(car);
+
+        }
+        cars.forEach(Car::carInfo);
     }
 }
