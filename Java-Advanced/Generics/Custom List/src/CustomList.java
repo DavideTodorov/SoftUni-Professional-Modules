b19bc7850941c1264a49fef2d190e9feb5c742db
Davide Todorov <davide.todorov@abv.bg>
2020-10-11 12:06:36 +0300

    Finished Custom List

--- Java-Advanced/Generics/Custom List/src/CustomList.java
+++ Java-Advanced/Generics/Custom List/src/CustomList.java
@@ -1,2 +1,67 @@
-public class CustomList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomList<T extends Comparable<T>> {
+
+    private List<T> listOfElements;
+
+    public CustomList() {
+        this.listOfElements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.listOfElements.add(element);
+    }
+
+    public T remove(int index) {
+        if (index >= 0 && index < this.listOfElements.size()) {
+            return this.listOfElements.remove(index);
+        }
+        return null;
+    }
+
+    public boolean contains(T element) {
+        return this.listOfElements.contains(element);
+    }
+
+    public void swap(int indexOne, int indexTwo) {
+        if (indexOne >= 0 && indexOne < this.listOfElements.size() &&
+                indexTwo >= 0 && indexTwo < this.listOfElements.size()) {
+
+            T firstElement = this.listOfElements.get(indexOne);
+            this.listOfElements.set(indexOne, this.listOfElements.get(indexTwo));
+            this.listOfElements.set(indexTwo, firstElement);
+        }
+    }
+
+    public int countGreaterThan(T element) {
+        int count = 0;
+        for (T currElement : listOfElements) {
+            if (currElement.compareTo(element) > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T getMax() {
+        return this.listOfElements.stream()
+                .max(Comparable::compareTo)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public T getMin() {
+        return this.listOfElements.stream()
+                .min(Comparable::compareTo)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T currElement : listOfElements) {
+            sb.append(currElement).append("\n");
+        }
+        return sb.toString();
+    }
 }
--- Java-Advanced/Generics/Custom List/src/Main.java
+++ Java-Advanced/Generics/Custom List/src/Main.java
@@ -1,6 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Use String as T
+        CustomList<String> customList = new CustomList<>();
 
+        String input = scanner.nextLine();
+        while (!"END".equals(input)) {
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+
+            switch (command) {
+                case "Add":
+                    String element = tokens[1];
+                    customList.add(element);
+                    break;
+
+                case "Remove":
+                    int index = Integer.parseInt(tokens[1]);
+                    customList.remove(index);
+                    break;
+
+                case "Contains":
+                    element = tokens[1];
+                    System.out.println(customList.contains(element));
+                    break;
+
+                case "Swap":
+                    int indexOne = Integer.parseInt(tokens[1]);
+                    int indexTwo = Integer.parseInt(tokens[2]);
+                    customList.swap(indexOne, indexTwo);
+                    break;
+
+                case "Greater":
+                    element = tokens[1];
+                    System.out.println(customList.countGreaterThan(element));
+                    break;
+
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+
+                case "Print":
+                    System.out.println(customList.toString());
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
     }
 }
