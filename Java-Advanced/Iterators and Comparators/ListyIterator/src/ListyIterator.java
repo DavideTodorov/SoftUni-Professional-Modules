d809da242b3f1b66d13a22e3b5990b2b1ecf5b0c
Davide Todorov <davide.todorov@abv.bg>
2020-10-15 17:52:16 +0300

    Create ListyIterator method

---
+++ Java-Advanced/Iterators and Comparators/ListyIterator/src/ListyIterator.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListyIterator  {
+
+    private int index = 0;
+    private List<String> list;
+
+    public ListyIterator(String... elements) {
+        this.list = Arrays.asList(elements);
+    }
+
+    public boolean hasNext() {
+
+        return index < list.size() - 1;
+    }
+
+    public boolean move() {
+        boolean isMoved = false;
+
+        if (index < list.size() - 1) {
+            index++;
+            isMoved = true;
+        }
+
+        return isMoved;
+    }
+
+    public String print() {
+        if (list.isEmpty() || index >= list.size()) {
+            throw new IndexOutOfBoundsException("Invalid Operation!");
+        }
+        return list.get(index);
+    }
+}
--- Java-Advanced/Iterators and Comparators/ListyIterator/src/Main.java
+++ Java-Advanced/Iterators and Comparators/ListyIterator/src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String[] tokens = scanner.nextLine().split("\\s+");
+        String[] toAdd = new String[tokens.length - 1];
+        int indexToAdd = 0;
+        for (int i = 1; i < tokens.length; i++) {
+            toAdd[indexToAdd++] = tokens[i];
+        }
+
+        ListyIterator listyIterator = new ListyIterator(toAdd);
+
+        String input = scanner.nextLine();
+        while (!"END".equals(input)) {
+
+            switch (input) {
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+
+                case "Print":
+                    System.out.println(listyIterator.print());
+                    break;
+            }
+
+            input = scanner.nextLine();
+        }
     }
 }
