0880e9f46ee92bf433ea19b2c1b813218a0cf403
Davide Todorov <davide.todorov@abv.bg>
2020-10-15 19:25:56 +0300

    Create Stack Class

--- Java-Advanced/Iterators and Comparators/Stack Iterator/src/Main.java
+++ Java-Advanced/Iterators and Comparators/Stack Iterator/src/Main.java
@@ -1,6 +1,40 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Stack stack = new Stack();
+
+        String input = scanner.nextLine();
+        while (!"END".equals(input)) {
+
+
+            switch (input.split("\\s+")[0]) {
+                case "Push":
+                    pushElements(input.substring(5), stack);
+                    break;
+
+                case "Pop":
+
+            }
+
+            input = scanner.nextLine();
+        }
+
+        for (int integer : stack) {
+            System.out.println();
+        }
+    }
+
+    private static void pushElements(String input, Stack stack) {
+        int[] ints = Arrays.stream(input.split(",\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        stack.push(ints);
     }
 }
--- Java-Advanced/Iterators and Comparators/Stack Iterator/src/Stack.java
+++ Java-Advanced/Iterators and Comparators/Stack Iterator/src/Stack.java
@@ -1,2 +1,46 @@
-public class Stack {
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Stack implements Iterable<Integer> {
+
+    private List<Integer> stack;
+
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+
+    public void push(int... integers) {
+        if (integers.length >= 1) {
+            for (int i = 0; i < integers.length; i++) {
+                stack.add(integers[i]);
+            }
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.remove(stack.size() - 1);
+        } else {
+            System.out.println("No elements");
+        }
+    }
+
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < stack.size();
+            }
+
+            @Override
+            public Integer next() {
+                return stack.get(index++);
+            }
+        };
+    }
+
 }
