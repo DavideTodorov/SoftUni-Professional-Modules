f782a4e4dfb5f23b6b1d79bc87a1e77a0e85b844
Davide Todorov <davide.todorov@abv.bg>
2020-10-17 08:21:12 +0300

    Finished Froggy

--- Java-Advanced/Iterators and Comparators/Froggy/src/Lake.java
+++ Java-Advanced/Iterators and Comparators/Froggy/src/Lake.java
@@ -1,2 +1,33 @@
-public class Lake {
+import java.util.Iterator;
+
+public class Lake implements Iterator<Integer> {
+    private int[] numbers;
+    private int index;
+
+
+    public Lake(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return this.index < this.numbers.length;
+    }
+
+    @Override
+    public Integer next() {
+        final int lastEvenIndex = numbers.length % 2 == 0 ? numbers.length - 2 : numbers.length - 1;
+
+        if (index == lastEvenIndex) {
+            int currNum = numbers[index];
+            index = 1;
+            return currNum;
+        }
+
+
+        int currNum = numbers[index];
+        index += 2;
+        return currNum;
+    }
 }
--- Java-Advanced/Iterators and Comparators/Froggy/src/Main.java
+++ Java-Advanced/Iterators and Comparators/Froggy/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = Arrays.stream(scanner.nextLine().split(",\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Lake lake = new Lake(numbers);
+
+        StringBuilder sb = new StringBuilder();
+        for (Lake it = lake; it.hasNext(); ) {
+            int number = it.next();
+            sb.append(number).append(", ");
+        }
+
+        System.out.println(sb.toString().substring(0, sb.toString().lastIndexOf(", ")));
     }
 }
