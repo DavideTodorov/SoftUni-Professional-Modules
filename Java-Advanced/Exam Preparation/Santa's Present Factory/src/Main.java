b29e6c37ff26f8fa742240b5a5ee373436a341c8
Davide Todorov <davide.todorov@abv.bg>
2020-10-24 14:56:18 +0300

    Finished Santa's Present Factory

--- Java-Advanced/Exam Preparation/Santa's Present Factory/src/Main.java
+++ Java-Advanced/Exam Preparation/Santa's Present Factory/src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -8,64 +6,73 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> materials = Arrays.stream(scanner.nextLine().split("\\s+"))
+        ArrayDeque<Integer> materialsStack = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .forEach(materialsStack::push);
+
+        ArrayDeque<Integer> magicLevelQueue = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toCollection(ArrayDeque::new));
 
-        ArrayDeque<Integer> magicLevel = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toCollection(ArrayDeque::new));
+
+        Map<String, Integer> toys = new TreeMap<>();
+
+        toys.put("Doll", 0);
+        toys.put("Wooden train", 0);
+        toys.put("Teddy bear", 0);
+        toys.put("Bicycle", 0);
 
 
-        int dolls = 0;
-        int woodenTrains = 0;
-        int teddyBears = 0;
-        int bicycles = 0;
+        while (!materialsStack.isEmpty() && !magicLevelQueue.isEmpty()) {
+            int currMaterial = materialsStack.pop();
+            int currMagicLevel = magicLevelQueue.poll();
 
-        while (!materials.isEmpty() && !magicLevel.isEmpty()) {
-            int currMaterial = materials.poll();
-            int currMagicLevel = magicLevel.pop();
+            int result = currMaterial * currMagicLevel;
+
+            if (result < 0) {
+                result = currMaterial + currMagicLevel;
+                materialsStack.push(result);
+                continue;
+            }
 
             if (currMaterial == 0 && currMagicLevel == 0) {
                 continue;
             }
 
             if (currMaterial == 0) {
-                magicLevel.offerFirst(currMagicLevel);
+                magicLevelQueue.offerFirst(currMagicLevel);
                 continue;
             }
 
             if (currMagicLevel == 0) {
-                materials.offerLast(currMaterial);
-                continue;
-            }
-
-            int result = currMaterial * currMagicLevel;
-
-            if (result < 0) {
-                result = currMaterial + currMagicLevel;
-                materials.offerLast(result);
+                materialsStack.push(currMaterial);
                 continue;
             }
 
             if (result == 150) {
-                dolls++;
+                toys.put("Doll", toys.get("Doll") + 1);
+
             } else if (result == 250) {
-                woodenTrains++;
+                toys.put("Wooden train", toys.get("Wooden train") + 1);
+
             } else if (result == 300) {
-                teddyBears++;
+                toys.put("Teddy bear", toys.get("Teddy bear") + 1);
+
             } else if (result == 400) {
-                bicycles++;
+                toys.put("Bicycle", toys.get("Bicycle") + 1);
+
             } else if (result > 0) {
                 currMaterial += 15;
-                materials.offerLast(currMaterial);
+                materialsStack.push(currMaterial);
             }
         }
 
-        if (dolls > 0 && woodenTrains > 0) {
+        if (toys.get("Doll") > 0 && toys.get("Wooden train") > 0) {
             System.out.println("The presents are crafted! Merry Christmas!");
 
-        } else if (teddyBears > 0 && bicycles > 0) {
+        } else if (toys.get("Teddy bear") > 0 && toys.get("Bicycle") > 0) {
             System.out.println("The presents are crafted! Merry Christmas!");
 
         } else {
@@ -73,10 +80,10 @@ public class Main {
 
         }
 
-        if (!materials.isEmpty()) {
+        if (!materialsStack.isEmpty()) {
             StringBuilder sb = new StringBuilder();
-            while (!materials.isEmpty()) {
-                sb.append(String.valueOf(materials.pollLast())).append(", ");
+            while (!materialsStack.isEmpty()) {
+                sb.append(String.valueOf(materialsStack.pop())).append(", ");
             }
 
             System.out.printf("Materials left: %s%n", String.join(", ",
@@ -84,29 +91,18 @@ public class Main {
 
         }
 
-        if (!magicLevel.isEmpty()) {
+        if (!magicLevelQueue.isEmpty()) {
             StringBuilder sb = new StringBuilder();
-            while (!magicLevel.isEmpty()) {
-                sb.append(String.valueOf(magicLevel.pollFirst())).append(", ");
+            while (!magicLevelQueue.isEmpty()) {
+                sb.append(String.valueOf(magicLevelQueue.poll())).append(", ");
             }
             System.out.printf("Magic left: %s%n", String.join(", ",
                     sb.toString().substring(0, sb.toString().lastIndexOf(", "))));
         }
 
-        if (bicycles > 0) {
-            System.out.printf("Bicycle: %d%n", bicycles);
-        }
-
-        if (dolls > 0) {
-            System.out.printf("Doll: %d%n", dolls);
-        }
-
-        if (teddyBears > 0) {
-            System.out.printf("Teddy bear: %d%n", teddyBears);
-        }
-
-        if (woodenTrains > 0) {
-            System.out.printf("Wooden rain: %d%n", woodenTrains);
-        }
+        toys.entrySet()
+                .stream()
+                .filter(t -> t.getValue() > 0)
+                .forEach(t -> System.out.printf("%s: %d%n", t.getKey(), t.getValue()));
     }
 }
