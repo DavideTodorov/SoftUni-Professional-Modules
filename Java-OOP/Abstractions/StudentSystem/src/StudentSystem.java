79d97d8e73e22fa457c358ef9ab5dfa4e4c2ae1e
Davide Todorov <davide.todorov@abv.bg>
2020-10-28 20:50:47 +0200

    Finished StudentSystem

--- Java-OOP/Abstractions/StudentSystem/src/StudentSystem.java
+++ Java-OOP/Abstractions/StudentSystem/src/StudentSystem.java
@@ -1,59 +1,51 @@
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class StudentSystem {
-    private Map<String, Student> repo;
 
-    public StudentSystem()
-    {
-        this.repo = new HashMap<>();
+    private Map<String, Student> students;
+
+    public StudentSystem() {
+        this.students = new HashMap<>();
     }
 
-    public Map<String, Student> getRepo() {
-        return this.repo;
+    public Map<String, Student> getStudents() {
+        return this.students;
     }
 
-    public void ParseCommand(String[]args)
-    {
-//        Scanner scanner = new Scanner(System.in);
-//        String[] args = scanner.nextLine().split(" ");
+    public void create(String[] arrayToParse) {
+        String name = arrayToParse[1];
+        int age = Integer.parseInt(arrayToParse[2]);
+        double grade = Double.parseDouble(arrayToParse[3]);
 
-        if (args[0].equals("Create"))
-        {
-            var name = args[1];
-            var age = Integer.parseInt(args[2]);
-            var grade =Double.parseDouble(args[3]);
-            if (!repo.containsKey(name))
-            {
-                var student = new Student(name, age, grade);
-                repo.put(name,student);
-            }
+        if (!students.containsKey(name)) {
+            Student student = new Student(name, age, grade);
+            students.put(name, student);
         }
-        else if (args[0].equals("Show"))
-        {
-            var name = args[1];
-            if (repo.containsKey(name))
-            {
-                var student = repo.get(name);
-                String view = String.format("%s is %s years old.",student.getName(),student.getAge());
+    }
 
-                if (student.getGrade() >= 5.00)
-                {
-                    view += " Excellent student.";
-                }
-                else if (student.getGrade() < 5.00 && student.getGrade() >= 3.50)
-                {
-                    view += " Average student.";
-                }
-                else
-                {
-                    view += " Very nice person.";
-                }
+    public void show(String[] tokens) {
+        String name = tokens[1];
+        if (students.containsKey(name)) {
+            Student student = students.get(name);
 
-                System.out.println(view);
-            }
+            StringBuilder result = new StringBuilder();
+
+            result.append(String.format("%s is %s years old.", student.getName(), student.getAge()));
+
+            appendTextForGrade(student, result);
+
+            System.out.println(result.toString());
+        }
+    }
+
+    private void appendTextForGrade(Student student, StringBuilder result) {
+        if (student.getGrade() >= 5.00) {
+            result.append(" Excellent student.");
+        } else if (student.getGrade() < 5.00 && student.getGrade() >= 3.50) {
+            result.append(" Average student.");
+        } else {
+            result.append(" Very nice person.");
         }
     }
 }
