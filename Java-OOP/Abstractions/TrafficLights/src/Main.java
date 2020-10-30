aa73bb55b5e46f09583d97167998c24aaabdc1bb
Davide Todorov <davide.todorov@abv.bg>
2020-10-30 14:15:08 +0200

    Finished TrafficLights

--- Java-OOP/Abstractions/TrafficLights/src/Main.java
+++ Java-OOP/Abstractions/TrafficLights/src/Main.java
@@ -1,11 +1,58 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private enum Light {
+        RED, GREEN, YELLOW;
 
-   
+        private static Light[] lights = values();
+
+        // implementation of next value
+        public Light next(){
+            return lights[(this.ordinal() + 1) % lights.length];
+        }
+    }
+
+    private static class TrafficLight {
+
+        private Light color;
+
+        //constructor
+        public TrafficLight(Light color){
+            this.color = color;
+        }
+        // next color method calls next value method of light
+        public void nextColor(){
+            this.color = color.next();
+        }
+
+        @Override
+        public String toString(){
+            return color.name();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // arraylist of trafficlights
+        List<TrafficLight> trafficLights = Arrays.stream(sc.nextLine().split("\\s+"))
+                .map(e -> new TrafficLight(Light.valueOf(e)))
+                .collect(Collectors.toList());
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        // cycle to switch lights and print to console
+        for (int i = 0; i < n; i++) {
+
+            trafficLights.forEach(TrafficLight::nextColor);
+            System.out.println(trafficLights.toString().replaceAll("[\\[\\],]", ""));
+
+        }
     }
 }
--- Java-OOP/Abstractions/TrafficLights/src/TrafficLight.java
+++
@@ -1,9 +0,0 @@
-public enum TrafficLight {
-    RED,
-    GREEN,
-    YELLOW;
-
-    public void update() {
-
-    }
-}
