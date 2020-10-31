d8bb48439732c5c629005aa48019060cfbbf7cd8
Davide Todorov <davide.todorov@abv.bg>
2020-10-31 20:04:48 +0200

    Finished RandomArrayList

---
+++ Java-OOP/Inheritance/RandomArrayList/src/RandomArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+
+    public T getRandomElement(){
+        Random random = new Random();
+        int randomIndex = random.nextInt(super.size());
+       return super.remove(randomIndex);
+    }
+}
