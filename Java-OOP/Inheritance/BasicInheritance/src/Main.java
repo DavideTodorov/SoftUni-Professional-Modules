fcede104ff94097f9ec18f4c14462bcc9ed4ee26
Davide Todorov <davide.todorov@abv.bg>
2020-10-31 19:59:18 +0200

    Finished HierarchicalInheritance

--- Java-OOP/Inheritance/BasicInheritance/src/Main.java
+++ Java-OOP/Inheritance/BasicInheritance/src/Main.java
@@ -2,10 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Puppy puppy = new Puppy();
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
 
-        puppy.eat();
-        puppy.bark();
-        puppy.weep();
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
     }
 }
