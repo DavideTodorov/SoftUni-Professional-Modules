2336cbf0092d5720987f80d2a33437d2ffc9fa70
Davide Todorov <davide.todorov@abv.bg>
2020-11-01 17:59:47 +0200

    Finished Coffee And Tea Classes

---
+++ Java-OOP/Inheritance/Restaurant/src/HotBeverage.java
@@ -0,0 +1,8 @@
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
