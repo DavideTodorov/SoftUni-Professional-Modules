10858fcf1e1ad3719e130a803fe1b80a529493c2
Davide Todorov <davide.todorov@abv.bg>
2020-11-01 10:44:33 +0200

    Created All Motorcycle Classes

---
+++ Java-OOP/Inheritance/NeedForSpeed/src/Vehicle.java
@@ -0,0 +1,50 @@
+public class Vehicle {
+    private final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public void drive(double kilometers) {
+        double fuelNeeded = kilometers * this.fuelConsumption;
+        if (fuelNeeded >= this.fuel) {
+            this.fuel -= fuelNeeded;
+        }
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public double getFuelConsumption() {
+        return this.fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return this.fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+
+}
