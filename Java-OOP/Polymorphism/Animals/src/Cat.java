f31401605243452fed0f07a807681025477a49fc
Davide Todorov <davide.todorov@abv.bg>
2020-11-11 18:26:07 +0200

    Finished Animals

---
+++ Java-OOP/Polymorphism/Animals/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+
+    protected Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + "MEEOW";
+    }
+}
