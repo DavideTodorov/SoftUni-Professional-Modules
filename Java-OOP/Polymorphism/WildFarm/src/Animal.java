f1f5b6041570230f719ca995929aca19a2891933
Davide Todorov <davide.todorov@abv.bg>
2020-11-12 15:34:30 +0200

    Create Mouse And Zebra Classes

---
+++ Java-OOP/Polymorphism/WildFarm/src/Animal.java
@@ -0,0 +1,18 @@
+public abstract class Animal {
+
+    protected String animalName;
+    protected String animalType;
+    protected Double animalWeight;
+    protected Integer foodEaten;
+
+    public Animal(String animalName, String animalType, Double animalWeight) {
+        this.animalName = animalName;
+        this.animalType = animalType;
+        this.animalWeight = animalWeight;
+        this.foodEaten = 0;
+    }
+
+    public abstract void makeSound();
+
+    public abstract void eat(Food food);
+}
---
+++ Java-OOP/Polymorphism/WildFarm/src/Mammal.java
@@ -0,0 +1,9 @@
+public abstract class Mammal extends Animal {
+
+    protected String livingRegion;
+
+    public Mammal(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight);
+        this.livingRegion = livingRegion;
+    }
+}
