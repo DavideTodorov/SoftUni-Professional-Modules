4695a087049482462e12a5b1806cd1a91fe5ff9a
Davide Todorov <davide.todorov@abv.bg>
2020-11-19 07:47:22 +0200

    Finished BlackBoxInteger

--- Java-OOP/Reflection And Annotations/BlackBoxInteger/src/Main.java
+++ Java-OOP/Reflection And Annotations/BlackBoxInteger/src/Main.java
@@ -1,5 +1,50 @@
-public class Main {
-    public static void main(String[] args) {
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException,
+            IllegalAccessException,
+            InvocationTargetException,
+            InstantiationException,
+            NoSuchFieldException {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        Class<BlackBoxInt> boxIntClass = BlackBoxInt.class;
+        Constructor<BlackBoxInt> constructor = boxIntClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        BlackBoxInt blackBoxInt = constructor.newInstance();
+
+
+        Method[] methods = boxIntClass.getDeclaredMethods();
+        Map<String, Method> methodsMap = new HashMap<>();
+
+        for (Method method : methods) {
+            methodsMap.put(method.getName(), method);
+        }
+
+
+        String input = scanner.nextLine();
+        while (!"END".equals(input)) {
+            String[] tokens = input.split("_");
+            String methodName = tokens[0];
+            int value = Integer.parseInt(tokens[1]);
+
+            Method method = methodsMap.get(methodName);
+            method.setAccessible(true);
+            method.invoke(blackBoxInt, value);
+
+            Field innerValue = boxIntClass.getDeclaredField("innerValue");
+            innerValue.setAccessible(true);
+            int innerValueInt = innerValue.getInt(blackBoxInt);
+            System.out.println(innerValueInt);
+
+
+            input = scanner.nextLine();
+        }
     }
 }
