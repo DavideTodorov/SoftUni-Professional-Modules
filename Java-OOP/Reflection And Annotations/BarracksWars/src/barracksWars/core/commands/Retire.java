7330598db1bfa5d0d66dcea11743c154abc317ea
Davide Todorov <davide.todorov@abv.bg>
2020-11-20 17:49:40 +0200

    Add new functionalities

---
+++ Java-OOP/Reflection And Annotations/BarracksWars/src/barracksWars/core/commands/Retire.java
@@ -0,0 +1,16 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
+
+public class Retire extends Command {
+
+    protected Retire(String[] data, Repository repository, UnitFactory unitFactory) {
+        super(data, repository, unitFactory);
+    }
+
+    @Override
+    public String execute() {
+        if (super.getRepository().)
+    }
+}
