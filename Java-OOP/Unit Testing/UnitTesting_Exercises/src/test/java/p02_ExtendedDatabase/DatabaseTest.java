99382e7452d7847cb00319443366b1a119f90169
Davide Todorov <davide.todorov@abv.bg>
2020-11-27 18:27:50 +0200

    Update DatabaseTest.java

--- Java-OOP/Unit Testing/UnitTesting_Exercises/src/test/java/p02_ExtendedDatabase/DatabaseTest.java
+++ Java-OOP/Unit Testing/UnitTesting_Exercises/src/test/java/p02_ExtendedDatabase/DatabaseTest.java
@@ -49,11 +49,13 @@ public class DatabaseTest {
         assertNull(personFound);
     }
 
+
     @Test(expected = OperationNotSupportedException.class)
     public void testFindingByNullUsername() throws OperationNotSupportedException {
         Database database = new Database(new Person(10, "Name"));
         database.findByUsername(null);
     }
+
 
     @Test(expected = OperationNotSupportedException.class)
     public void testFindingByNotExistingId() throws OperationNotSupportedException {
@@ -68,6 +70,7 @@ public class DatabaseTest {
         database.findById(3);
     }
 
+
     @Test
     public void testFindingBYExistingId() throws OperationNotSupportedException {
         Person person1 = Mockito.mock(Person.class);
@@ -81,5 +84,70 @@ public class DatabaseTest {
         Person byId = database.findById(2);
 
         assertEquals(person2, byId);
+    }
+
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testAddingNullToDatabase() throws OperationNotSupportedException {
+        Database database = new Database(new Person(13, "Name"));
+        database.add(null);
+    }
+
+
+    @Test
+    public void testAddingValidPersonToDatabase() throws OperationNotSupportedException {
+        Database database = new Database(new Person(13, "Name"));
+        Person person = new Person(72, "Name2");
+        database.add(person);
+        Person[] elements = database.getElements();
+        assertEquals(person, elements[1]);
+    }
+
+
+    @Test
+    public void testRemovingPeopleFromDatabase() throws OperationNotSupportedException {
+        Database database =
+                new Database(new Person(1, "Name1"), new Person(2, "Name2"));
+
+        database.remove();
+        int length = database.getElements().length;
+        assertEquals(1, length);
+
+        database.remove();
+        length = database.getElements().length;
+        assertEquals(0, length);
+    }
+
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testRemovingWhenThereAreNoPeopleLeftInDatabase() throws OperationNotSupportedException {
+        Database database =
+                new Database(new Person(1, "Name1"), new Person(2, "Name2"));
+
+        database.remove();
+        int length = database.getElements().length;
+        assertEquals(1, length);
+
+        database.remove();
+        length = database.getElements().length;
+        assertEquals(0, length);
+
+        database.remove();
+    }
+
+
+    @Test
+    public void testFindExistingPersonByUsername() throws OperationNotSupportedException {
+        Person person = new Person(13, "Name");
+        Person person2 = new Person(42, "Name2");
+        Person person3 = new Person(72, "Name3");
+
+        Database database = new Database(person, person2, person3);
+
+        Person foundPerson1 = database.findByUsername("Name");
+        Person foundPerson2 = database.findByUsername("Name3");
+
+        assertEquals(person, foundPerson1);
+        assertEquals(person3, foundPerson2);
     }
 }
