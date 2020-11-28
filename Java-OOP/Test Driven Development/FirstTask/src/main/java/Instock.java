917b8579e4a334cb1d901171387bab412c213d36
Davide Todorov <davide.todorov@abv.bg>
2020-11-28 15:15:38 +0200

    Update

--- Java-OOP/Test Driven Development/FirstTask/src/main/java/Instock.java
+++ Java-OOP/Test Driven Development/FirstTask/src/main/java/Instock.java
@@ -6,61 +6,60 @@ public class Instock implements ProductStock {
 
     @Override
     public int getCount() {
-        throw new UnsupportedOperationException();
+        return 0;
     }
 
     @Override
     public boolean contains(Product product) {
-        throw new UnsupportedOperationException();
+        return false;
     }
 
     @Override
     public void add(Product product) {
-        throw new UnsupportedOperationException();
+
     }
 
     @Override
     public void changeQuantity(String product, int quantity) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public Product find(int index) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Product findByLabel(String label) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Iterable<Product> findFirstByAlphabeticalOrder(int count) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Iterable<Product> findAllInRange(double lo, double hi) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Iterable<Product> findAllByPrice(double price) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Iterable<Product> findFirstMostExpensiveProducts(int count) {
-        throw new UnsupportedOperationException();
+       return null;
     }
 
     @Override
     public Iterable<Product> findAllByQuantity(int quantity) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 
     @Override
     public Iterator<Product> iterator() {
-        throw new UnsupportedOperationException();
+        return null;
     }
 }
