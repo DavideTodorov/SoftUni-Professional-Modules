ade7ab8ed7e54c5d9bf1dc21e3a4d493d201dcdc
Davide Todorov <davide.todorov@abv.bg>
2020-11-28 15:57:42 +0200

    Add new tests and method implementations

--- Java-OOP/Test Driven Development/FirstTask/src/main/java/Instock.java
+++ Java-OOP/Test Driven Development/FirstTask/src/main/java/Instock.java
@@ -1,31 +1,51 @@
 import jdk.jshell.spi.ExecutionControl;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Instock implements ProductStock {
 
+    private List<Product> products;
+
+    public Instock() {
+        products = new ArrayList<>();
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return products.size();
     }
 
     @Override
     public boolean contains(Product product) {
-        return false;
+        return products.contains(product);
     }
 
     @Override
     public void add(Product product) {
-
+        products.add(product);
     }
 
     @Override
     public void changeQuantity(String product, int quantity) {
+        Product productInStock = null;
+        for (Product product1 : products) {
+            if (product1.getLabel().equals(product)){
+                productInStock = product1;
+            }
+        }
+
+        if (productInStock == null){
+            throw new IllegalArgumentException();
+        }
+
+        productInStock.setQuantity(quantity);
     }
 
     @Override
     public Product find(int index) {
-        return null;
+        return products.get(index);
     }
 
     @Override
@@ -50,7 +70,7 @@ public class Instock implements ProductStock {
 
     @Override
     public Iterable<Product> findFirstMostExpensiveProducts(int count) {
-       return null;
+        return null;
     }
 
     @Override
--- Java-OOP/Test Driven Development/FirstTask/src/test/java/ProductStockTest.java
+++ Java-OOP/Test Driven Development/FirstTask/src/test/java/ProductStockTest.java
@@ -1,11 +1,84 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ProductStockTest {
+    private ProductStock products;
+
+    @Before
+    public void setUp() {
+        products = new Instock();
+    }
 
     @Test
-    public void test(){
+    public void testAddingProducts() {
+        Product product = new Product("test_label", 1.0, 1);
+        products.add(product);
+        boolean contains = products.contains(product);
+        assertTrue(contains);
+    }
 
+
+    @Test
+    public void testReturnsCorrectCountOfProducts() {
+        Product product1 = new Product();
+        Product product2 = new Product();
+        Product product3 = new Product();
+
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+
+        int count = products.getCount();
+
+        assertEquals(3, count);
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testFindingByInvalidIndex() {
+        products.add(new Product());
+        products.find(1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testFindingByNegativeIndex() {
+        products.find(-1);
+    }
+
+    @Test
+    public void testFindingByValidIndex() {
+        Product product1 = new Product("label_1", 3.00, 1);
+        Product product2 = new Product("label_2", 3.00, 1);
+        Product product3 = new Product("label_3", 3.00, 1);
+
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+
+        Product productFound = products.find(1);
+        assertEquals(product2, productFound);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testChangingQuantityToNoneExistingProduct() {
+        Product product = new Product("label_1", 3.00, 1);
+
+        products.add(product);
+
+        products.changeQuantity("not_existing_label", 10);
+    }
+
+    @Test
+    public void testChangingQuantityToExistingProduct() {
+        Product product = new Product("label_1", 3.00, 1);
+
+        products.add(product);
+        products.changeQuantity("label_1", 10);
+        Product modifiedProduct = products.find(0);
+
+        assertEquals(10, modifiedProduct.getQuantity());
     }
 }
