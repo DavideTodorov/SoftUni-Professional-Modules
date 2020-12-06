4387b6a7c01036ba66f58a910e41a8ce63cfecf5
Davide Todorov <davide.todorov@abv.bg>
2020-12-06 11:24:23 +0200

    Update FieldImpl.java

--- Java-OOP/Exam Preparation/CounterStriker/src/CounterStriker/models/field/FieldImpl.java
+++ Java-OOP/Exam Preparation/CounterStriker/src/CounterStriker/models/field/FieldImpl.java
@@ -16,16 +16,56 @@ public class FieldImpl implements Field {
         List<Player> counterTerrorists = new ArrayList<>();
 
         for (Player player : playerList) {
-            if (player.getClass().getSimpleName().equals("Terrorist")){
+            if (player.getClass().getSimpleName().equals("Terrorist")) {
                 terrorists.add(player);
-            }else {
+            } else {
                 counterTerrorists.add(player);
             }
         }
 
-        
+        boolean hasWinner = false;
+
+        while (!hasWinner) {
+            //TERRORISTS ATTACK
+            for (Player terrorist : terrorists) {
+                for (Player counterTerrorist : counterTerrorists) {
+                    counterTerrorist.takeDamage(terrorist.getGun().fire());
+                }
+            }
+
+            boolean cTAreAlive = false;
+            for (Player counterTerrorist : counterTerrorists) {
+                if (counterTerrorist.isAlive()) {
+                    cTAreAlive = true;
+                    break;
+                }
+            }
+
+            if (!cTAreAlive) {
+                return "Terrorist wins!";
+            }
 
 
+            //COUNTER TERRORISTS ATTACK
+            for (Player counterTerrorist : counterTerrorists) {
+                for (Player terrorist : terrorists) {
+                    terrorist.takeDamage(counterTerrorist.getGun().fire());
+                }
+            }
+
+
+            boolean tAreAlive = false;
+            for (Player terrorist : terrorists) {
+                if (terrorist.isAlive()) {
+                    tAreAlive = true;
+                    break;
+                }
+            }
+
+            if (!tAreAlive) {
+                return "Counter Terrorist wins!";
+            }
+        }
 
         return null;
     }
