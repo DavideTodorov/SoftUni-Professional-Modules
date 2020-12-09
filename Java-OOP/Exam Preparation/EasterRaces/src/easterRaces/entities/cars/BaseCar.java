32650c8c61e61d3b74d72b9e8b1d3801e6eb2d5e
Davide Todorov <davide.todorov@abv.bg>
2020-12-09 07:40:16 +0200

    Finished Car implementation

--- Java-OOP/Exam Preparation/EasterRaces/src/easterRaces/entities/cars/BaseCar.java
+++ Java-OOP/Exam Preparation/EasterRaces/src/easterRaces/entities/cars/BaseCar.java
@@ -1,5 +1,7 @@
 package easterRaces.entities.cars;
 
+import static easterRaces.common.ExceptionMessages.*;
+
 public abstract class BaseCar implements Car {
     private String model;
     private int horsePower;
@@ -12,37 +14,52 @@ public abstract class BaseCar implements Car {
     }
 
     //SETTERS
-    private void setCubicCentimeters(double cubicCentimeters) {
-        this.cubicCentimeters = cubicCentimeters;
+    private void setModel(String model) {
+        if (model == null || model.trim().isEmpty() || model.length() < 4) {
+            throw new IllegalArgumentException(String.format(INVALID_MODEL, model));
+        }
+
+        this.model = model;
     }
 
     private void setHorsePower(int horsePower) {
+        if (this.getClass().getSimpleName().equals("MuscleCar")) {
+            if (horsePower < 400 || horsePower > 600) {
+                throw new IllegalArgumentException(String.format(INVALID_HORSE_POWER, horsePower));
+            }
+        } else {
+            if (horsePower < 250 || horsePower > 450) {
+                throw new IllegalArgumentException(String.format(INVALID_HORSE_POWER, horsePower));
+            }
+        }
+
         this.horsePower = horsePower;
     }
 
-    private void setModel(String model) {
-        this.model = model;
+    private void setCubicCentimeters(double cubicCentimeters) {
+        this.cubicCentimeters = cubicCentimeters;
     }
 
 
     //METHODS FROM CAR INTERFACE
     @Override
     public String getModel() {
-        return null;
+        return this.model;
     }
 
     @Override
     public int getHorsePower() {
-        return 0;
+        return this.horsePower;
     }
 
     @Override
     public double getCubicCentimeters() {
-        return 0;
+        return this.cubicCentimeters;
     }
 
     @Override
     public double calculateRacePoints(int laps) {
-        return 0;
+        double result = Math.round(cubicCentimeters / horsePower * laps);
+        return Math.round(result);
     }
 }
