924f172b48a79252028bee082a021f5da3047119
Davide Todorov <davide.todorov@abv.bg>
2020-12-09 07:23:58 +0200

    Create DriverImpl.java

---
+++ Java-OOP/Exam Preparation/EasterRaces/src/easterRaces/entities/drivers/DriverImpl.java
@@ -0,0 +1,47 @@
+package easterRaces.entities.drivers;
+
+import easterRaces.entities.cars.Car;
+
+public class DriverImpl implements Driver {
+    private String name;
+    private Car car;
+    private int numberOfWins;
+    private boolean canParticipate;
+
+    public DriverImpl(String name) {
+        this.name = name;
+        numberOfWins = 0;
+        car = null;
+        canParticipate = false;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Car getCar() {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfWins() {
+        return 0;
+    }
+
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void winRace() {
+
+    }
+
+    @Override
+    public boolean getCanParticipate() {
+        return false;
+    }
+}
