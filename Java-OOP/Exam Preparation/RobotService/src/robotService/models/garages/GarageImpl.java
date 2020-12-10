edc63286d849e65c5c0c0adae546d5e9832b96f7
Davide Todorov <davide.todorov@abv.bg>
2020-12-10 07:18:49 +0200

    Add GarageImpl

---
+++ Java-OOP/Exam Preparation/RobotService/src/robotService/models/garages/GarageImpl.java
@@ -0,0 +1,34 @@
+package robotService.models.garages;
+
+import robotService.models.garages.interfaces.Garage;
+import robotService.models.robots.interfaces.Robot;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class GarageImpl implements Garage {
+    private int capacity;
+    private Map<String, Robot> robots;
+
+    public GarageImpl() {
+        this.capacity = 10;
+        this.robots = new LinkedHashMap<>();
+    }
+
+    
+
+    @Override
+    public Map<String, Robot> getRobots() {
+        return null;
+    }
+
+    @Override
+    public void manufacture(Robot robot) {
+
+    }
+
+    @Override
+    public void sell(String robotName, String ownerName) {
+
+    }
+}
--- Java-OOP/Exam Preparation/RobotService/src/robotService/models/procedures/Work.java
+++ Java-OOP/Exam Preparation/RobotService/src/robotService/models/procedures/Work.java
@@ -5,6 +5,6 @@ import robotService.models.robots.interfaces.Robot;
 public class Work extends BaseProcedure {
     @Override
     public void doService(Robot robot, int procedureTime) {
-        
+
     }
 }
