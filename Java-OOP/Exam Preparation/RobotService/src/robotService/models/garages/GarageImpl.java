ecd14a39720129d2f938963f9eda9acae19b3b2a
Davide Todorov <davide.todorov@abv.bg>
2020-12-10 11:35:37 +0200

    Finish GarageImpl

--- Java-OOP/Exam Preparation/RobotService/src/robotService/models/garages/GarageImpl.java
+++ Java-OOP/Exam Preparation/RobotService/src/robotService/models/garages/GarageImpl.java
@@ -6,6 +6,8 @@ import robotService.models.robots.interfaces.Robot;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static robotService.common.ExceptionMessages.*;
+
 public class GarageImpl implements Garage {
     private static final int CAPACITY = 10;
     private Map<String, Robot> robots;
@@ -14,20 +16,37 @@ public class GarageImpl implements Garage {
         this.robots = new LinkedHashMap<>();
     }
 
-    
 
     @Override
     public Map<String, Robot> getRobots() {
-        return null;
+        return this.robots;
     }
 
     @Override
     public void manufacture(Robot robot) {
+        if (robots.size() >= 10) {
+            throw new IllegalStateException(NOT_ENOUGH_CAPACITY);
+        }
 
+        String robotName = robot.getName();
+
+        if (robots.containsKey(robotName)) {
+            throw new IllegalArgumentException(String.format(EXISTING_ROBOT, robotName));
+        }
+
+        robots.put(robotName, robot);
     }
 
     @Override
     public void sell(String robotName, String ownerName) {
+        if (!robots.containsKey(robotName)){
+            throw new IllegalArgumentException(String.format(NON_EXISTING_ROBOT,robotName));
+        }
 
+        Robot robot = robots.get(robotName);
+        robot.setOwner(ownerName);
+        robot.setBought(true);
+        
+        robots.remove(robotName);
     }
 }
