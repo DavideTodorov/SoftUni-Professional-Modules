afbacb1f0dc8371cc682164ad143b3940fbb644c
Davide Todorov <davide.todorov@abv.bg>
2020-12-11 07:18:48 +0200

    Create BaseComputer

--- Java-OOP/Exam Preparation/OnlineShop/src/onlineShop/models/products/peripherals/BasePeripheral.java
+++ Java-OOP/Exam Preparation/OnlineShop/src/onlineShop/models/products/peripherals/BasePeripheral.java
@@ -11,7 +11,7 @@ public abstract class BasePeripheral extends BaseProduct {
                 overallPerformance);
         this.connectionType = connectionType;
     }
-    
+
     @Override
     public String toString() {
         return "BasePeripheral{}";
