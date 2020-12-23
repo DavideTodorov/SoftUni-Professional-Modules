ea3b1b952a6748085a203d25148594a02b554733
Davide Todorov <davide.todorov@abv.bg>
2020-12-23 07:47:56 +0200

    Finished RecursiveArraySum

--- Java-Advanced/Workshop/Algorithms/RecursiveArraySum/src/Main.java
+++ Java-Advanced/Workshop/Algorithms/RecursiveArraySum/src/Main.java
@@ -1,2 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = calculateArrSum(arr, 0);
+
+        System.out.println(sum);
+    }
+
+    private static int calculateArrSum(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return arr[index];
+        }
+
+        return arr[index] + calculateArrSum(arr, index + 1);
+    }
 }
