a32daa8224ac02c2e53f68ef9699e4a031c4acd5
Davide Todorov <davide.todorov@abv.bg>
2020-12-23 07:54:24 +0200

    Finished RecursiveFactorial

--- Java-Advanced/Workshop/Algorithms/RecursiveFactorial/src/Main.java
+++ Java-Advanced/Workshop/Algorithms/RecursiveFactorial/src/Main.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int factorial = findFactorial(number);
+
+        System.out.println(factorial);
+    }
+
+    private static int findFactorial(int number) {
+        if (number == 1) {
+            return number;
+        }
+
+        return number * findFactorial(number - 1);
+    }
 }
