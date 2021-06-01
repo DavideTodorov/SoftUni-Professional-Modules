80018eb8699df00e93ec091488993ec09d3ea33b
Davide Todorov <davide.todorov@abv.bg>
2021-06-01 18:45:01 +0300

    Refactor geography_database

--- Databases/MySQL/Built-in Functions/geography_database.sql
+++ Databases/MySQL/Built-in Functions/geography_database.sql
@@ -812,11 +812,13 @@ ORDER BY `iso_code`;
 
 
 -- 11.Mix of Peak and River Names
-
-
-
-
-
-
-
-
+SELECT 
+    `peak_name`,
+    `river_name`,
+    LOWER(CONCAT(`peak_name`, SUBSTR(`river_name`, 2))) AS 'mix'
+FROM
+    `peaks`,
+    `rivers`
+WHERE
+    RIGHT(LOWER(`peak_name`), 1) = LEFT(LOWER(`river_name`), 1)
+ORDER BY `mix`;
