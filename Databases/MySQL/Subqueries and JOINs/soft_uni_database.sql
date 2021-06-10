a7e52ea033369fcbc5432d44b5fca8138328cee5
Davide Todorov <davide.todorov@abv.bg>
2021-06-10 11:14:34 +0300

    Finished TD(2) soft_uni_database.sql

--- Databases/MySQL/Subqueries and JOINs/soft_uni_database.sql
+++ Databases/MySQL/Subqueries and JOINs/soft_uni_database.sql
@@ -1674,6 +1674,8 @@ INSERT INTO `towns` (`town_id`, `name`) VALUES
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 
 
+/* Exersices from Tasks Description(1) */
+
 
 -- 1. Managers
 SELECT 
@@ -1713,3 +1715,183 @@ WHERE `manager_id` IS NULL;
 SELECT COUNT(*)
 FROM `employees`
 WHERE `salary` > (SELECT AVG(`salary`) FROM `employees`);
+
+
+/* Exersices from Tasks Description(2) */
+
+
+-- 01. Employee Address
+SELECT 
+    `e`.`employee_id`,
+    `e`.`job_title`,
+    `e`.`address_id`,
+    `a`.`address_text`
+FROM
+    `employees` AS `e`
+        JOIN
+    `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+ORDER BY `e`.`address_id`
+LIMIT 5;
+
+
+
+-- 02. Addresses with Towns
+SELECT 
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `t`.`name` AS `town`,
+    `a`.`address_text`
+FROM
+    `employees` AS `e`
+        JOIN
+    `addresses` AS `a` ON `e`.`address_id` = `a`.`address_id`
+        JOIN
+    `towns` AS `t` ON `a`.`town_id` = `t`.`town_id`
+ORDER BY `e`.`first_name` , `e`.`last_name`
+LIMIT 5;
+
+
+
+-- 03. Sales Employee
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `d`.`name` AS `department_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+        AND `d`.`name` = 'Sales'
+ORDER BY `e`.`employee_id` DESC
+LIMIT 5;
+
+
+
+-- 04. Employee Departments
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `e`.`salary`,
+    `d`.`name` AS `department_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE
+    `e`.`salary` > 15000
+ORDER BY `e`.`department_id` DESC
+LIMIT 5;
+
+
+
+-- 05. Employees Without Project
+SELECT 
+    `e`.`employee_id`, `e`.`first_name`
+FROM
+    `employees` AS `e`
+        LEFT JOIN
+    `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+WHERE
+    `ep`.`project_id` IS NULL
+ORDER BY `employee_id` DESC
+LIMIT 3;
+
+
+
+-- 06. Employees Hired After
+SELECT 
+    `e`.`first_name`,
+    `e`.`last_name`,
+    `e`.`hire_date`,
+    `d`.`name` AS `dept_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+WHERE
+    `d`.`name` IN ('Sales' , 'Finance')
+        AND `e`.`hire_date` > '1999-01-01'
+ORDER BY `e`.`hire_date` ASC;
+
+
+
+-- 07. Employees with Project
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `p`.`name` AS `project_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+        JOIN
+    `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
+WHERE
+    `p`.`start_date` > '2002-08-13'
+        AND `p`.`end_date` IS NULL
+ORDER BY `e`.`first_name` ASC , `project_name` ASC
+LIMIT 5;
+
+
+
+-- 08. Employee 24
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    IF(YEAR(`p`.`start_date`) >= '2005',
+        NULL,
+        `p`.`name`) AS `project_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `employees_projects` AS `ep` ON `e`.`employee_id` = `ep`.`employee_id`
+        JOIN
+    `projects` AS `p` ON `ep`.`project_id` = `p`.`project_id`
+WHERE
+    `e`.`employee_id` = 24
+ORDER BY `project_name`;
+
+
+
+-- 09. Employee Manager
+SELECT 
+    `e`.`employee_id`,
+    `e`.`first_name`,
+    `e`.`manager_id`,
+    `e2`.`first_name` AS `manager_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `employees` AS `e2` ON `e`.`manager_id` = `e2`.`employee_id`
+WHERE
+    `e`.`manager_id` IN (3 , 7)
+ORDER BY `e`.`first_name`;
+
+
+
+-- 10. Employee Summary
+SELECT 
+    `e`.`employee_id`,
+    CONCAT(`e`.`first_name`, ' ', `e`.`last_name`) AS `employee_name`,
+    CONCAT(`e2`.`first_name`, ' ', `e2`.`last_name`) AS `manager_name`,
+    `d`.`name` AS `department_name`
+FROM
+    `employees` AS `e`
+        JOIN
+    `employees` AS `e2` ON `e`.`manager_id` = `e2`.`employee_id`
+        JOIN
+    `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+ORDER BY `e`.`employee_id`
+LIMIT 5;
+
+
+
+-- 11. Min Average Salary
+SELECT 
+    AVG(`salary`) AS `min_average_salary`
+FROM
+    `employees`
+GROUP BY `department_id`
+ORDER BY `min_average_salary`
+LIMIT 1;
