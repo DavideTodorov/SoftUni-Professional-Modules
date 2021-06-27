c56df856fb73fe3b16178dfad725451d90dba95f
Davide Todorov <davide.todorov@abv.bg>
2021-06-27 15:56:06 +0300

    Refactor Minions from Exercise

--- Databases/Spring Data/Database Access with JDBC/Minions/src/IncreaseMinionsAge.java
+++ Databases/Spring Data/Database Access with JDBC/Minions/src/IncreaseMinionsAge.java
@@ -9,21 +9,27 @@ public class IncreaseMinionsAge {
 
         Scanner scanner = new Scanner(System.in);
 
-        Object[] input = Arrays.stream(scanner.nextLine().split("\\s+")).toArray();
+        int[] input = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
 
         Connection connection = DriverManager.getConnection
                 ("jdbc:mysql://localhost:3306/minions_db", "root", "12345");
 
 
-        PreparedStatement minionsStatement = connection.prepareStatement("SELECT if(id IN (?), lower(name), name),\n" +
-                "       if(id IN (?), age + 1, age)\n" +
+        PreparedStatement minionsStatement = connection.prepareStatement("SELECT if(id IN (?,?,?), lower(name), name),\n" +
+                "       if(id IN (?, ?, ?), age + 1, age)\n" +
                 "FROM minions;");
 
 
         // TODO: Feature not supported in MySQL
-        minionsStatement.setArray(1, connection.createArrayOf("INT", input));
-        minionsStatement.setArray(2, connection.createArrayOf("INT", input));
+        minionsStatement.setInt(1, input[0]);
+        minionsStatement.setInt(2, input[1]);
+        minionsStatement.setInt(3, input[2]);
+        minionsStatement.setInt(4, input[0]);
+        minionsStatement.setInt(5, input[1]);
+        minionsStatement.setInt(6, input[2]);
 
 
         ResultSet alteredMinions = minionsStatement.executeQuery();
